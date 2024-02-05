7b8565a54f6246e74844b5df6a895f37f3a9ed1f
hesteban-tuenti <hector.estebanramos@telefonica.com>
2024-02-05 10:08:06 +0100

    better print

--- scripts/pushealo.sh
+++ scripts/pushealo.sh
@@ -14,18 +14,31 @@ function get_branch() {
 
 function get_staged_files() {
     FILES_STAGED=($(git diff --cached --name-only))
-    echo "Staged files: ${FILES_STAGED[@]}"
+    if [ ${#FILES_STAGED[@]} -eq 0 ]; then
+        echo "No files staged"
+        staged=false
+    else
+        staged=true
+        echo "Staged files:"
+        for file in "${FILES_STAGED[@]}"
+        do
+            echo "  - $file"
+        done
+    fi
 }
 
 function get_modified_files() {
     FILES_MODIFIED=($(git status --porcelain | grep '^ M' | awk '{print $2}'))
-    echo "Modified files: ${FILES_MODIFIED[@]}"
+    echo "Modified files:"
+    for file in "${FILES_MODIFIED[@]}"
+    do
+        echo "  - $file"
+    done
 }
 
 function unstage_files_to_keep() {
     for file in "${FILES_STAGED[@]}"
     do
-        echo "File: $file"
         if [[ " ${FILES_TO_KEEP[@]} " =~ " ${file} " ]]; then
             echo "Removing from stage file: $file"
             git reset HEAD $file > /dev/null 2>&1
@@ -36,15 +49,15 @@ function unstage_files_to_keep() {
 function checkout_files_to_keep() {
     for file in "${FILES_MODIFIED[@]}"
     do
-        echo "File: $file"
         if [[ " ${FILES_TO_KEEP[@]} " =~ " ${file} " ]]; then
-            echo "Checking out file: $file"
+            echo "Checking out: $file"
             git checkout -- $file 2>/dev/null
         fi
     done
 }
 
 function commit_changed_files(){
+    echo "Committing changes"
     git add -u
     read -p "Commit message: " message
     git commit -m "$message"
@@ -58,6 +71,9 @@ function push_branch() {
 change_dir
 get_modified_files
 get_staged_files
+if [ $staged ]; then
+    unstage_files_to_keep
+fi
 unstage_files_to_keep
 checkout_files_to_keep
 commit_changed_files
